feae2ee4c97ca976f7155d498af1e2352bd66c08
hussenot <hussenot@>
2024-06-27 11:20:07 +0200

    Termination script for the 42 iterations

---
+++ script9-50.sh
@@ -0,0 +1,85 @@
+cd ~/kilonovae-H0/lcsimu/1perday9/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday9.dat
+cd ~/kilonovae-H0/lcsimu/1perday10/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday10.dat
+cd ~/kilonovae-H0/lcsimu/1perday11/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday11.dat
+cd ~/kilonovae-H0/lcsimu/1perday12/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday12.dat
+cd ~/kilonovae-H0/lcsimu/1perday13/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday13.dat
+cd ~/kilonovae-H0/lcsimu/1perday14/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday14.dat
+cd ~/kilonovae-H0/lcsimu/1perday15/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday15.dat
+cd ~/kilonovae-H0/lcsimu/1perday16/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday16.dat
+cd ~/kilonovae-H0/lcsimu/1perday17/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday17.dat
+cd ~/kilonovae-H0/lcsimu/1perday18/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday18.dat
+cd ~/kilonovae-H0/lcsimu/1perday19/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday19.dat
+cd ~/kilonovae-H0/lcsimu/1perday20/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday20.dat
+cd ~/kilonovae-H0/lcsimu/1perday21/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday21.dat
+cd ~/kilonovae-H0/lcsimu/1perday22/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday22.dat
+cd ~/kilonovae-H0/lcsimu/1perday23/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday23.dat
+cd ~/kilonovae-H0/lcsimu/1perday24/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday24.dat
+cd ~/kilonovae-H0/lcsimu/1perday25/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday25.dat
+cd ~/kilonovae-H0/lcsimu/1perday26/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday26.dat
+cd ~/kilonovae-H0/lcsimu/1perday27/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday27.dat
+cd ~/kilonovae-H0/lcsimu/1perday28/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday28.dat
+cd ~/kilonovae-H0/lcsimu/1perday29/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday29.dat
+cd ~/kilonovae-H0/lcsimu/1perday30/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday30.dat
+cd ~/kilonovae-H0/lcsimu/1perday31/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday31.dat
+cd ~/kilonovae-H0/lcsimu/1perday32/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday32.dat
+cd ~/kilonovae-H0/lcsimu/1perday33/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday33.dat
+cd ~/kilonovae-H0/lcsimu/1perday34/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday34.dat
+cd ~/kilonovae-H0/lcsimu/1perday35/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday35.dat
+cd ~/kilonovae-H0/lcsimu/1perday36/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday36.dat
+cd ~/kilonovae-H0/lcsimu/1perday37/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday37.dat
+cd ~/kilonovae-H0/lcsimu/1perday38/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday38.dat
+cd ~/kilonovae-H0/lcsimu/1perday39/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday39.dat
+cd ~/kilonovae-H0/lcsimu/1perday40/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday40.dat
+cd ~/kilonovae-H0/lcsimu/1perday41/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday41.dat
+cd ~/kilonovae-H0/lcsimu/1perday42/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday42.dat
+cd ~/kilonovae-H0/lcsimu/1perday43/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday43.dat
+cd ~/kilonovae-H0/lcsimu/1perday44/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday44.dat
+cd ~/kilonovae-H0/lcsimu/1perday45/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday45.dat
+cd ~/kilonovae-H0/lcsimu/1perday46/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday46.dat
+cd ~/kilonovae-H0/lcsimu/1perday47/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday47.dat
+cd ~/kilonovae-H0/lcsimu/1perday48/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday48.dat
+cd ~/kilonovae-H0/lcsimu/1perday49/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday49.dat
+cd ~/kilonovae-H0/lcsimu/1perday50/grizyJHK/
+mpiexec -n 1 lightcurve-analysis --model Bu2019lm --svd-path /home/thussenot/nmma/svdmodels --outdir outdirBNSBu32cores --label Simu2019lm --prior ./Bu2019lm_AT170817.prior --tmin 0.01 --tmax 26 --dt 0.01  --nlive 2048 --Ebv-max 0 --trigger-time 60000 --plot --xlim 0,15 --ylim 24,15 --bestfit --data ../170817bestsim1perday50.dat
+
