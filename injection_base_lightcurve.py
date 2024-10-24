cc1d44dacf076fb53151668e192de9775c757967
Thomas Hussenot <thussenot@login01.expanse.sdsc.edu>
2024-10-24 05:07:15 -0700

    Simulated lightcurves of neighbouring OFFGRID point in configuration space

--- injection_base_lightcurve.py
+++ injection_base_lightcurve.py
@@ -12,15 +12,19 @@ from nmma.em.model import SVDLightCurveModel
 
 sample_times = np.arange(0.1,15,0.1)
 filts = ['ps1__g','ps1__r','ps1__i','ps1__z','ps1__y']
-params={'KNphi':60,'log10_mej_dyn':np.log(0.010),'log10_mej_wind':np.log(0.05),
-        'KNtheta':np.arccos(0.8)*180/np.pi,'luminosity_distance':40}
+#params={'KNphi':60,'log10_mej_dyn':np.log(0.010),'log10_mej_wind':np.log(0.05),
+#        'KNtheta':np.arccos(0.8)*180/np.pi,'luminosity_distance':40}
+params={'KNphi':53,'log10_mej_dyn':np.log(0.007),'log10_mej_wind':np.log(0.04),
+        'KNtheta':np.arccos(0.75)*180/np.pi,'luminosity_distance':40}
+
+name = 'InjectionBaseLightcurve2'
 model = SVDLightCurveModel('Bu2019lm', sample_times, svd_path='/home/thussenot/nmma/svdmodels/',
                            interpolation_type='sklearn_gp',filters=filts)
 lbol,mAB = model.generate_lightcurve(sample_times,params)
 ps1 = Table(mAB)
 for f in ps1.keys():
     ps1[f] += 5*np.log10(params['luminosity_distance']*(10**6 /10))
-ps1.write('/home/thussenot/kilonovae-H0/InjectionBaseLightcurve1_ps1.dat',format='ascii.csv',overwrite=True)
+ps1.write('/home/thussenot/kilonovae-H0/Injection_Recovery_IterationsVariation/'+name+'/'+name+'_ps1.dat',format='ascii.csv',overwrite=True)
 
 filts = ['2massj','2massh','2massks']
 model = SVDLightCurveModel('Bu2019lm', sample_times, svd_path='/home/thussenot/nmma/svdmodels/',
@@ -29,4 +33,4 @@ lbol,mAB = model.generate_lightcurve(sample_times,params)
 mass = Table(mAB)
 for f in mass.keys():
     mass[f] += 5*np.log10(params['luminosity_distance']*(10**6 /10))
-mass.write('/home/thussenot/kilonovae-H0/InjectionBaseLightcurve1_mass.dat',format='ascii.csv',overwrite=True)
+mass.write('/home/thussenot/kilonovae-H0/Injection_Recovery_IterationsVariation/'+name+'/'+name+'_mass.dat',format='ascii.csv',overwrite=True)
