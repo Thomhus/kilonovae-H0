47f65432ec94c50ff7bcb76a056d025c246ecee6
hussenot <hussenot@>
2026-04-03 15:59:48 +0200

    Resuls of AT2017gfo runs analysed with Bu2025MLP and Bu2026MLP models

---
+++ 170817/Bu2025MLP/sequence.sh
@@ -0,0 +1,10 @@
+cd ErrfreeDefault
+mpiexec -n 7 bash ../nmma_instruction.sh
+cd ../1Default
+mpiexec -n 7 bash ../nmma_instruction.sh
+cd ../1Angleradio
+mpiexec -n 7 bash ../nmma_instruction.sh
+cd ../1AngleradioGWdistance
+mpiexec -n 7 bash ../nmma_instruction.sh
+cd ../1AngleradioGWdistance+masses
+mpiexec -n 7 bash ../nmma_instruction.sh
